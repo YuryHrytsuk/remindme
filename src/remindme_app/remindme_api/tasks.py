b3e961b08498f1e46ac1c241d9ee0d09a3c3db5a
YuryHrytsuk <Yury_Hrytsuk1@epam.com>
2020-09-06 11:01:38 +0300

    Add retry

--- src/remindme_app/remindme_api/tasks.py
+++ src/remindme_app/remindme_api/tasks.py
@@ -1,19 +1,26 @@
 from __future__ import absolute_import, unicode_literals
 
+from smtplib import SMTPException
+
 from django.core.mail import send_mail
 
 from remindme_app import celery_app, settings
 from . import models
 
 
-@celery_app.task
-def send_reminder(reminder_id):
+@celery_app.task(bind=True, max_retries=3)
+def send_reminder(self, reminder_id):
     reminder = models.Reminder.objects.get(id=reminder_id)
     recipients = [reminder.author.email] + [user.email for user in reminder.cc_recipients.all()]
-    send_mail(
-        subject=reminder.header,
-        message=reminder.description,
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=recipients
-    )
-    reminder.delete()
+
+    try:
+        send_mail(
+            subject=reminder.header,
+            message=reminder.description,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=recipients
+        )
+    except SMTPException as e:
+        self.retry(exc=e, countdown=2 ** self.request.retries)
+    else:
+        reminder.delete()
